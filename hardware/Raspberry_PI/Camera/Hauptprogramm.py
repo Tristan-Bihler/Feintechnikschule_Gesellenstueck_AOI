420566575fa6759f8bebd6c629c90b45a87fd462
Tristan Bihler <tristanbihler@outlook.com>
2024-01-25 18:19:57 +0100

    Positionierung mit einprogrammiert

--- hardware/Raspberry_PI/Camera/Hauptprogramm.py
+++ hardware/Raspberry_PI/Camera/Hauptprogramm.py
@@ -4,19 +4,24 @@ import subprocess
 program_name = 'Vergleicher.py'
 
 
-# Function to sign in a user with email and password
+# Funktion für das einloggen und abfragen der daten, sowie ein scipt auzuführen
 def login_user(email, password):
     try:
         user = auth.sign_in_with_email_and_password(email, password)
         print(f"User signed in successfully. User ID: {user['localId']}")
 
-        # Example: Retrieve data from the 'users' node after successful login
+        # nach login datenbank abfragen und daten entnemen
         data = db.child("machines").child("lumina_mk1").get()
         print("Data from the 'users' node:")
         print(data)
         for test in data.each(): 
             print(test.val())
             if test.val() == 1:
+                try:
+                    positioner = r'hardware\\Raspberry_PI\\Camera\\positioning.py'  # Replace with the path to your actual script
+                    subprocess.run(["python", positioner])
+                except subprocess.CalledProcessError as e:
+                    print(f"Error: {e}")
                 try:
                     similarity_script = r'hardware\\Raspberry_PI\\Camera\\Vergleicher.py'
                     subprocess.run(['python', similarity_script], check=True)
@@ -29,7 +34,7 @@ def login_user(email, password):
         print(f"Login failed. Error: {e}")
 
 if __name__ == "__main__":
-    # Example: Login a user and retrieve data
+    # firebase verküpfungsadaten
     firebase_config = {
                     "apiKey": "AIzaSyCst7QRNnFR9eC0YpiDYjYuAyjsrDdUtT4",
                     "authDomain": "tbstudios-aoi.firebaseapp.com",
@@ -41,12 +46,9 @@ if __name__ == "__main__":
                     "measurementId": "G-G28PMSQPBE"
                 }
 
-    # Initialize Firebase
     firebase = pyrebase.initialize_app(firebase_config)
 
-    # Get a reference to the authentication service
     auth = firebase.auth()
 
-    # Get a reference to the database
     db = firebase.database()
     login_user("tristanbihler@outlook.com", "123456")
